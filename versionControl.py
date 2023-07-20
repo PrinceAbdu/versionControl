f8269d21f422e5f11450eb5acd403a11067a3d98
PrinceAbdu <140012952+PrinceAbdu@users.noreply.github.com>
2023-07-19 21:29:01 -0400

    Add files via upload

---
+++ versionControl.py
@@ -0,0 +1,42 @@
+def encode(password):
+    result = ''
+    for digit in password:
+        new_digit = str((int(digit) + 3) % 10)
+        result += new_digit
+    return result
+
+
+def decode(encoded_pass):
+    decoded_pass = ''
+    for digit in encoded_pass:
+        decoded_digit = str((int(digit) - 3) % 10)
+        decoded_pass += decoded_digit
+    return decoded_pass
+
+
+def main():
+    while True:
+        print("Menu")   #menu option
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+
+        option = input("Please enter an option: ")  #user input
+
+        if option == "1":
+            value = input("Please enter your password to encode: ")     #encoded password
+            encoded_pass = encode(value)
+            print("Your password has been encoded and stored!")
+            print()
+        elif option == "2":
+            print("The encoded password is", encoded_pass, "and the original password is", decode(encoded_pass))
+            #decoding password
+        elif option == "3":
+            break
+        else:
+            print("Invalid input")
+
+
+if __name__ == "__main__":
+    main()
